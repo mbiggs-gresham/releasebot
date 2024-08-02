7eaebb7c77e48d966c9482c6f8088e79de1bfa43
Matt Biggs <mbiggs@greshamtech.com>
2024-08-02 14:24:06 +0100

    Update metadata

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ export async function run(): Promise<void> {
     const ms: string = core.getInput('milliseconds')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`-> Waiting ${ms} milliseconds ...`)
 
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
